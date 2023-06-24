4e0a72580c6e41b9c9810a006a15a5514ae70f90
ammarali0608 <ammar.bscsf19@iba-suk.edu.pk>
2023-06-24 07:11:44 -0400

    Creating Recipies

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,45 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Create Recipe</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const title = e.target.title.value;
+          const description = e.target.description.value;
+          const ingredients = e.target.ingredients.value;
+          const instructions = e.target.instructions.value;
+          const image = e.target.image.files[0].name;
+          if (!title || !description || !ingredients || !instructions || !image)
+            return alert("Please fill all the fields");
+
+          const response = await fetch("http://localhost:3005/recipe/create", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              title,
+              description,
+              ingredients,
+              instructions,
+              image,
+            }),
+          });
+          const data = await response.json();
+          console.log(data);
+          fetchRecipes();
+        }}
+      >
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="description" placeholder="Description" />
+        {/* ingredients can be multiple */}
+        <input type="text" name="ingredients" placeholder="Ingredients" />
+
+        <input type="text" name="instructions" placeholder="Instructions" />
+        <input type="file" name="image" />
+        <button type="submit">Create</button>
+      </form>
       <h1>Recipies</h1>
       <RecipeList recipes={recipes} />
     </div>
